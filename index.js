9dedf0a4f31ec6003a0441220942184185e88ff4
EduardaPires <pireseduarda526@gmail.com>
2023-10-26 19:39:59 -0300

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// window.addEventListener("scroll", () => {
+//     const image = document.getElementById("base.png");
+//     const scrollY = window.scrollY;
+//     const translateY = -scrollY / 2; // Ajuste o valor para controlar a velocidade da subida
+
+//     image.style.transform = `translateY(${translateY}px)`;
+// });
