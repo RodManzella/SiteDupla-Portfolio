874933ae22069e60795e48f43ce01234bc58ed97
EduardaPires <pireseduarda526@gmail.com>
2023-10-26 20:32:01 -0300

    commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-// window.addEventListener("scroll", () => {
-//     const image = document.getElementById("base.png");
-//     const scrollY = window.scrollY;
-//     const translateY = -scrollY / 2; // Ajuste o valor para controlar a velocidade da subida
-
-//     image.style.transform = `translateY(${translateY}px)`;
-// });
+window.addEventListener("scroll", function() {
+    // Verifica se a página foi rolada até um ponto específico (por exemplo, 100 pixels)
+    if (window.scrollY >= 100) {
+        // Adiciona a classe .scroll-ativo ao body
+        document.body.classList.add("scroll-ativo");
+    } else {
+        // Remove a classe .scroll-ativo se a rolagem for reversa
+        document.body.classList.remove("scroll-ativo");
+    }
+});
