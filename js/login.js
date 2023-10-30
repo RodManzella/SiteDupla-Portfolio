539bc695a86c5fc2ed7f129379f65ebbea72c5ae
RodManzella <manzellateixeira@gmail.com>
2023-10-30 08:31:21 -0300

    commit

--- js/login.js
+++ js/login.js
@@ -20,7 +20,8 @@ Parse.initialize(
     const usuario = await query.first();
     if (usuario) {
       console.log("Login successful!");
-      document.title = "Dug´s - " + usuario.get("Username");
+      document.title = "Dug´s - " + usernameInput;
+      document.getElementById("pessoa-logada").innerHTML = usernameInput;
       window.location.href = "paginaLogado.html";
     } else {
       console.log("Invalid username or password.");
